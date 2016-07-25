e6ff0ef896a7574a2021e56d141d22a7a493d19d
valenti <Valentin Iliescu>
2016-07-25 14:05:58 -0700

    Refactor: Create separate class for input parser

--- StringCalculatorLibrary/StringCalculator.cs
+++ StringCalculatorLibrary/StringCalculator.cs
@@ -1,33 +1,16 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace StringCalculatorLibrary
 {
     public static class StringCalculator
     {
-        private static readonly char[] DefaultSeparators = { ',', '\n'};
-
         public static int Add(string numbersString)
         {
-            var tokens = Tokenize(numbersString);
-
+            var tokens = InputParser.Tokenize(numbersString);
 
             return tokens
                     .Select(int.Parse)
                     .Sum();
-        }
-
-        private static string[] Tokenize(string numbersString)
-        {
-            var separators = DefaultSeparators;
-            if (numbersString.StartsWith("//"))
-            {
-                numbersString = numbersString.Substring(3);
-                separators = separators.Union(new[] {numbersString[2]}).ToArray();
-            }
-
-            return numbersString
-                .Split(separators, StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
