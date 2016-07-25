7b1b5b382eb0641548763334c62225115894c005
valenti <Valentin Iliescu>
2016-07-25 12:51:18 -0700

    Added unit tests

---
+++ StringCalculatorLibrary.Tests/StringCalculatorTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculatorLibrary.Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
