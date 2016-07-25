9907bb83437d74c92e60b938342795211d06185a
valenti <Valentin Iliescu>
2016-07-25 13:40:38 -0700

    Added failing test for inline delimiter

--- StringCalculatorLibrary.Tests/StringCalculatorTests.cs
+++ StringCalculatorLibrary.Tests/StringCalculatorTests.cs
@@ -29,5 +29,11 @@ namespace StringCalculatorLibrary.Tests
         {
             StringCalculator.Add("1\n2,3").Should().Be(6);
         }
+
+        [TestMethod]
+        public void AddingNumbersWithDelimiterSpecifiedInlineShouldReturnTheSumOfThem()
+        {
+            StringCalculator.Add("//;\n1;2").Should().Be(3);
+        }
     }
 }
