6440b5971f6f974dc60032f0028bdae1d8f37e5c
valenti <viliescu@hotmail.com>
2016-09-19 18:15:27 -0700

    Make AddOneNumberShouldReturnTheNumber pass

--- StringCalculatorLibrary/StringCalculator.cs
+++ StringCalculatorLibrary/StringCalculator.cs
@@ -1,12 +1,22 @@
 using System;
+using System.Globalization;
 
 namespace StringCalculatorLibrary
 {
     public static class StringCalculator
     {
-        public static int Add(string empty)
+        public static int Add(string numbersString)
         {
-            return 0;
+            int result;
+            if (numbersString == string.Empty)
+            {
+                result = 0;
+            }
+            else
+            {
+                result = int.Parse(numbersString, NumberStyles.Integer, CultureInfo.InvariantCulture);
+            }
+            return result;
         }
     }
 }
