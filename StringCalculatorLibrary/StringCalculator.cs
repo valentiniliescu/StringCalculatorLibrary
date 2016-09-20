5334023441dcf745c2562be9a67bbe1df0796175
valenti <viliescu@hotmail.com>
2016-09-20 06:45:51 -0700

    Make AddThreeNumbersShouldReturnTheirSum pass

--- StringCalculatorLibrary/StringCalculator.cs
+++ StringCalculatorLibrary/StringCalculator.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Globalization;
+using System.Linq;
 
 namespace StringCalculatorLibrary
 {
@@ -6,27 +8,10 @@ namespace StringCalculatorLibrary
     {
         public static int Add(string numbersString)
         {
-            int result;
-            if (numbersString == string.Empty)
-            {
-                result = 0;
-            }
-            else
-            {
-                var separatorIndex = numbersString.IndexOf(',');
-                if (separatorIndex > -1)
-                {
-                    var number1 = int.Parse(numbersString.Substring(0, separatorIndex), NumberStyles.Integer, CultureInfo.InvariantCulture);
-                    var number2 = int.Parse(numbersString.Substring(separatorIndex + 1), NumberStyles.Integer, CultureInfo.InvariantCulture);
-
-                    result = number1 + number2;
-                }
-                else
-                {
-                    result = int.Parse(numbersString, NumberStyles.Integer, CultureInfo.InvariantCulture);
-                }
-            }
-            return result;
+            return numbersString
+                .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(numberString => int.Parse(numberString, NumberStyles.Integer, CultureInfo.InvariantCulture))
+                .Sum();
         }
     }
 }
