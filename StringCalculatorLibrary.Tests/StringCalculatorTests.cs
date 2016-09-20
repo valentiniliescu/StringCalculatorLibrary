0847434e55fee2ace4aa35eb9918f1450bac13a2
valenti <viliescu@hotmail.com>
2016-09-20 09:51:43 -0700

    Added AddNumbersWithSpecifiedDelimiterShouldReturnTheirSum failing test

--- StringCalculatorLibrary.Tests/StringCalculatorTests.cs
+++ StringCalculatorLibrary.Tests/StringCalculatorTests.cs
@@ -35,5 +35,11 @@ namespace StringCalculatorLibrary.Tests
         {
             StringCalculator.Add("1\n2,3").Should().Be(6);
         }
+
+        [TestMethod]
+        public void AddNumbersWithSpecifiedDelimiterShouldReturnTheirSum()
+        {
+            StringCalculator.Add("//;\n1;2").Should().Be(3);
+        }
     }
 }
