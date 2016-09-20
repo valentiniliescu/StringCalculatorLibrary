fa18056c78b7b2255fcce67571bdc7b9edb717a4
valenti <viliescu@hotmail.com>
2016-09-20 06:50:27 -0700

    Refactoring of the Add Method, extracted the separator into a readonly field

--- StringCalculatorLibrary/StringCalculator.cs
+++ StringCalculatorLibrary/StringCalculator.cs
@@ -6,10 +6,12 @@ namespace StringCalculatorLibrary
 {
     public static class StringCalculator
     {
+        private static readonly char[] Separators = { ',', '\n' };
+
         public static int Add(string numbersString)
         {
             return numbersString
-                .Split(new[] { ',', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Split(Separators, StringSplitOptions.RemoveEmptyEntries)
                 .Select(numberString => int.Parse(numberString, NumberStyles.Integer, CultureInfo.InvariantCulture))
                 .Sum();
         }
