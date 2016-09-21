3958d4e823088ad06209f0f3bd0c33ad3b366573
valenti <viliescu@hotmail.com>
2016-09-20 21:49:32 -0700

    Refactored out ParseInfo class

--- StringCalculatorLibrary/StringCalculator.cs
+++ StringCalculatorLibrary/StringCalculator.cs
@@ -6,26 +6,12 @@ namespace StringCalculatorLibrary
 {
     public static class StringCalculator
     {
-        private static readonly char[] DefaultSeparators = { ',', '\n' };
-
         public static int Add(string input)
         {
-            string numbersString;
-            char[] separators;
+            var parseInfo = new ParseInfo(input);
 
-            if (input.StartsWith("//"))
-            {
-                numbersString = input.Substring(4);
-                separators = DefaultSeparators.Concat(new[] {input[2]}).ToArray();
-            }
-            else
-            {
-                numbersString = input;
-                separators = DefaultSeparators;
-            }
-
-            return numbersString
-               .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+            return parseInfo.NumbersString
+               .Split(parseInfo.Separators, StringSplitOptions.RemoveEmptyEntries)
                .Select(numberString => int.Parse(numberString, NumberStyles.Integer, CultureInfo.InvariantCulture))
                .Sum();
         }
